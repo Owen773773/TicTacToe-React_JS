f4dd0a3a1a042c6e9a8af2360af103f74fa9ffae
Owen-Lianggara <6182301025@student.unpar.ac.id>
2025-12-24 15:42:02 +0700

    game logic done

--- src/Components/Game/Game.tsx
+++ src/Components/Game/Game.tsx
@@ -4,20 +4,39 @@ import {useState} from "react";
 function Game() {
     const[board, setBoard] = useState<("X" | "O" | null)[]>(Array(9).fill(null));
     const [player, setPlayer] = useState<null | "X" | "O">("X");
+    const [winner, setWinner] = useState<null | "X" | "O">(null);
 
     function handlePlay(index: number): void {
-        if (board[index] !== null) return;
+        if (board[index] !== null || winner !== null) return;
 
-        setBoard(
-            () => {
-                const nextBoard = [...board];
-                nextBoard[index] = player;
-                return nextBoard;
-            }
-        );
+        const nextBoard = [...board];
+        nextBoard[index] = player;
+
+        setBoard(nextBoard);
+        checkWinner(nextBoard);
 
         if (player === "X") setPlayer("O");
         else setPlayer("X");
+    }
+
+    function checkWinner(nextBoard: (null | "X" | "O")[]) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (nextBoard[a] && nextBoard[a] === nextBoard[b] && nextBoard[a] === nextBoard[c]) {
+                setWinner(nextBoard[a]);
+            }
+        }
     }
 
     return (
@@ -27,6 +46,7 @@ function Game() {
                 currentBoard={board}
                 onPlay={handlePlay}
             />
+            {winner && <p>Winner: {winner}</p>}
         </>
     );
 }
